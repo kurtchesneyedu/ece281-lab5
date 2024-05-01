12732630a9747943fe0170e2b84b0241a50e63b9
kurtchesneyedu <c26kurt.chesney@afacademy.af.edu>
2024-04-30 21:43:34 -0600

    added topbasys3

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -26,12 +26,75 @@ library ieee;
 
 
 entity top_basys3 is
--- TODO
+    port(
+        clk : in std_logic;
+        btnU : in std_logic;
+        btnC : in std_logic;
+        btnR : in std_logic;
+        sw : in std_logic_vector (15 downto 0);
+        
+        led : out std_logic_vector(15 downto 0);
+        seg : out std_logic_vector(7 downto 0)
+        );
+        
 end top_basys3;
 
 architecture top_basys3_arch of top_basys3 is 
   
-	-- declare components and signals
+	component ALU is
+        Port ( i_op : in STD_LOGIC_VECTOR (3 downto 0);
+               i_A : in STD_LOGIC_VECTOR (7 downto 0);
+               i_B : in STD_LOGIC_VECTOR (7 downto 0);
+               
+               o_result : out STD_LOGIC_VECTOR (7 downto 0);
+               o_sign : out STD_LOGIC;
+               o_cout : out STD_LOGIC;
+               o_zero : out STD_LOGIC);
+    end component ALU;
+    
+    component TDM4 is
+        generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
+        Port ( i_clk        : in  STD_LOGIC;
+               i_reset        : in  STD_LOGIC; -- asynchronous
+               i_D3         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D2         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D1         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D0         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               o_data        : out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               o_sel        : out STD_LOGIC_VECTOR (3 downto 0)    -- selected data line (one-cold)
+        );
+    end component TDM4;
+    
+    component clock_divider is
+        generic ( constant k_DIV : natural := 2    ); -- How many clk cycles until slow clock toggles
+                                                   -- Effectively, you divide the clk double this 
+                                                   -- number (e.g., k_DIV := 2 --> clock divider of 4)
+        port (  i_clk    : in std_logic;
+                i_reset  : in std_logic;           -- asynchronous
+                o_clk    : out std_logic           -- divided (slow) clock
+        );
+    end component clock_divider;
+    
+    component controller_fsm is
+        Port ( i_reset : in STD_LOGIC;
+               i_adv : in STD_LOGIC;
+               o_cycle : out STD_LOGIC_VECTOR (3 downto 0));
+    end component controller_fsm;
+    
+    component sevenSegDecoder is
+        Port ( i_D : in STD_LOGIC_VECTOR (3 downto 0);
+               o_S : out STD_LOGIC_VECTOR (6 downto 0));
+    end component sevenSegDecoder;
+    
+    entity twoscomp_decimal is
+        port (
+            i_binary: in std_logic_vector(7 downto 0);
+            o_negative: out std_logic;
+            o_hundreds: out std_logic_vector(3 downto 0);
+            o_tens: out std_logic_vector(3 downto 0);
+            o_ones: out std_logic_vector(3 downto 0)
+        );
+    end twoscomp_decimal;
 
   
 begin
